c74704016d6d075f04fc1d8871030fa5d24e974f
sihsob <bcsihsobhon@gmail.com>
2017-03-18 20:16:06 -0400

    Implemented transformation into EG tree

--- generate.py
+++ generate.py
@@ -1,14 +1,27 @@
-import statement
-import existential_statement
-import parse_tree
+from statement import squash_tree, print_tree
+from existential_statement import *
+from parse_tree import parse_sentence
+
+# Note this only takes in one line at a time, so for multiple premises, need to add in an implementation to combine into a single tree
 
 logic_statement = raw_input('Input a well-formed logic statement (No quantifiers): ')
 
 # First parse the logic statement
-start_tree = parse_tree.parse_sentence(logic_statement)
+start_tree = parse_sentence(logic_statement)
 
 # Squash AND statement in the tree
 stack = []
-s = statement.squash_tree(start_tree, stack)
+s = squash_tree(start_tree, stack)
 squashed_tree = s.pop()
-statement.print_tree(squashed_tree)
+print_tree(squashed_tree)
+
+# Transform the tree into an Existential graph tree
+stack = []
+e = transform(squashed_tree, stack)
+stack_len = len(e)
+sa = SheetAssertion(0, [])
+for i in range(0, stack_len):
+    prev_statement = e.pop()
+    sa.add_children(prev_statement)
+eg_tree = sa
+print_eg_tree(eg_tree)
